788f27a8d0516ab0a4711a8345e99fa640d49766
Lukasz Fuszara <lukasz@fuszara.pl>
2021-04-03 14:19:26 +0200

    user roles - small fix

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,15 +5,15 @@ class Ability
   def initialize(user)
     if !user
       can [:index, :show], :all
-    elsif user
-      can [:new, :create, :edit, :update, :destroy], Topic do |topic|
-        topic.user_id.include? @user.id
-      end
-      can [:new, :create, :edit, :update, :destroy], Comment do |comment|
-        comment.user_id.include? @user.id
-      end
     elsif user.superadmin_role?
       can [:index, :show, :new, :create, :edit, :update, :destroy], :all
+    elsif user
+      can [:index, :show, :new, :create, :edit, :update, :destroy], Topic do |topic|
+        topic.user_id.include? @user.id
+      end
+      can [:index, :show, :new, :create, :edit, :update, :destroy], Comment do |comment|
+        comment.user_id.include? @user.id
+      end
     end
   end
 
